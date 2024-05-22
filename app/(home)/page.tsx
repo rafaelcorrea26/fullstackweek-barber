d7a6ec237b9a3a20d181766ff379166b45b747ea
rafaelcorrea26 <rafael.fs.camaqua@gmail.com>
2024-05-21 22:50:22 -0300

    feat: add search page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,15 @@
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import Header from "../../components/ui/header";
+import Header from "../../components/header";
 import Search from "./components/search";
+import BookingItem from "@/components/booking-item";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, Rafael!</h2>
+        <h2 className="text-xl font-bold">Olá, Ana!</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
@@ -17,6 +18,10 @@ export default function Home() {
       </div>
       <div className="px-5 mt-6">
         <Search />
+      </div>
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem />
       </div>
     </div>
   );

ffdfe8a809b2b4c569e8c555a5239f20a4faf644
rafaelcorrea26 <rafael.fs.camaqua@gmail.com>
2024-05-21 21:49:58 -0300

    feat: add search components

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/app/_components/ui/button";
-import { Card, CardContent } from "@/app/_components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 
